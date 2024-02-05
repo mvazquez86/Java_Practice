50b79c7bb8bf2de5c5a1c438d3ad83069868b8cb
marcelavazquez <vazquezmarcela13@gmail.com>
2024-02-04 19:02:03 -0700

    Testing Github

--- src/day06_practice_tasks/MathUtility.java
+++ src/day06_practice_tasks/MathUtility.java
@@ -11,7 +11,16 @@ public class MathUtility {
 
         int square = square (7);
         System.out.println(square);
-    }
+
+        double squareDouble = square (7.5);
+        System.out.println(squareDouble);
+
+        int cube = square(3);
+        System.out.println(cube);
+
+        double cubeDouble = cube(3.5);
+        System.out.println(cubeDouble);
+     }
 
     public static double calculate(double x, char operator, double y) {
 
@@ -47,6 +56,7 @@ public class MathUtility {
             case '+' -> result = x + y;
             case '-' -> result = x - y;
             case '/' -> result = x / y;
+            case '*' -> result = x * y;
         }
 
         return result;
@@ -55,6 +65,21 @@ public class MathUtility {
     public static int square(int num){
      int square = calculate(num, '*', num);
         return square;
+    }
+
+    public static double square(double num){
+        double squareInt = calculate(num, '*', num);
+        return squareInt;
+    }
+
+    public static int cube (int num){
+        int cube = square(num)*num;
+        return cube;
+    }
+
+    public static double cube (double num){
+        double cube = square(num)*num;
+        return cube;
     }
 
 }
